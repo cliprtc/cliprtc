c3d33f8e791b09f7b5e8069fedd1118efccb56a1
lete114 <me@imlete.cn>
2025-12-11 18:23:54 +0800

    fix: sidebar i18n

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -2,7 +2,6 @@ import type { WindowsValue } from '@/types/win'
 import { getCurrentWindow } from '@tauri-apps/api/window'
 import { createTray } from '@/hooks/tray'
 import { Windows } from '@/types/win'
-import { CMDS } from './cmds'
 import { initWindows } from './window'
 
 const win = getCurrentWindow()
